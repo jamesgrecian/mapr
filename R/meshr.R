f4dbd01a4e14e98a520282a92b83fb14492ea879
James Grecian <james.grecian@gmail.com>
2021-01-05 16:30:49 +0000

    Address TopologyException error

--- R/meshr.R
+++ R/meshr.R
@@ -71,8 +71,11 @@ meshr <- function(dat,
     }
 
     # load in shapefile from rworldmap, clip to north or south and project
+    # Load in shapefile from rworldmap, clip to north or south and project
     world_shp <- sf::st_as_sf(rworldmap::countriesLow)
-    world_shp <- sf::st_crop(world_shp, CP)
+    suppressWarnings(
+      world_shp <- sf::st_crop(sf::st_buffer(world_shp, 0), CP)
+      )
     world_shp <- sf::st_transform(world_shp, prj) %>% sf::st_buffer(0)
 
     # convert data to sf and project
